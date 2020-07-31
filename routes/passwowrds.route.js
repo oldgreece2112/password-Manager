0273620c6da0ce998a8a9db2f2125b11b4565bae
Alex Sofianidis <ansofianidis@outlook.com>
2020-07-30 20:28:08 -0500

    password encryption added

--- routes/passwowrds.route.js
+++ routes/passwowrds.route.js
@@ -1,10 +1,66 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var crypto = require("crypto");
 var Password = require('../models/password.model');
+var User = require("../models/user.model");
 var router = express.Router();
 
 router.get("/create", function(req, res){
-    res.render("passwords/create", {currentUser:req.user});
+    res.render("passwords/create");
+});
+
+router.post("/create", function(req, res){
+    var url = req.body.url;
+    var username = req.body.username;
+    var length = req.user.settings.passwordLength;
+    var password = "";
+    var picker = 0;
+    while(password.length < length){
+        picker = Math.floor(Math.random() * 3) + 1
+
+        if (picker == 1 && req.user.settings.letters){
+            password = password + String.fromCharCode(Math.floor(Math.random() * (122- 65)) + 65);
+        }
+
+        if (picker == 2 && req.user.settings.specialCharacters){
+            password = password + String.fromCharCode(Math.floor(Math.random() * (47 - 33)) + 33);
+        }
+
+        if (picker == 3 && req.user.settings.numbers){
+            password = password + String.fromCharCode(Math.floor(Math.random() * (57 - 48)) + 48);
+        }
+    }
+
+    var newPassword = {
+        url: url,
+        username: username,
+        password: password
+    };
+
+    User.findOne({username: req.user.username}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            Password.create(newPassword, function(err, newPassword){
+                if(err){
+                    console.log(err);
+                }else{
+                    var cipher = crypto.createCipher('aes128', foundUser._id.toString());
+                    var encrypted = cipher.update(password, 'utf8', 'hex');
+                    encrypted += cipher.final('hex');
+                    newPassword.password = encrypted;
+                    newPassword.save();
+                    foundUser.passwords.push(newPassword);
+                    foundUser.save();
+                    console.log(newPassword);
+                    console.log(foundUser);
+                    res.redirect('/dashboard');
+                }
+            });
+        }
+    });
+
+    
 });
 
 module.exports = router;
